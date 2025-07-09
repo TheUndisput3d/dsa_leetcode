b9e002be9c8098c5854dddf5045dadb2e6bf3ca7
Aayush <theundisputed.ze@gmail.com>
2025-07-09 15:42:46 +0545

    add infix_to_prefix gfg

---
+++ stack/infix_postfix_prefix/infix_to_postfix.py
@@ -0,0 +1,33 @@
+class Solution:
+    def precedence(self, ch):
+        if ch == "^":
+            return 5
+        elif ch == "*" or ch == "/":
+            return 3
+        elif ch == "+" or ch == "-":
+            return 2
+        else:
+            return 1
+            
+    def InfixtoPostfix(self, s):
+        stack = []
+        result = []
+    
+        for c in s:
+            if ("a" <= c <= "z") or ("A" <= c <= "Z") or ("0" <= c <= "9"):
+                result.append(c)
+            elif c == "(":
+                stack.append(c)
+            elif c == ")":
+                while stack and stack[-1] != "(":
+                    result.append(stack.pop())
+                stack.pop()
+            else:
+                while stack and self.precedence(c) <= self.precedence(stack[-1]):
+                    result.append(stack.pop())
+                stack.append(c)
+    
+        while stack:
+            result.append(stack.pop())
+    
+        return "".join(result)
