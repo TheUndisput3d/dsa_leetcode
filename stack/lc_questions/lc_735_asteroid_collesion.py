497b7110c8f8806930dfa957d6bd78875704128e
Aayush <theundisputed.ze@gmail.com>
2025-07-10 19:00:42 +0545

    add lc asteroid collesion implementation

---
+++ stack/lc_questions/lc_735_asteroid_collesion.py
@@ -0,0 +1,19 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for i in range(len(asteroids)):
+            curr = asteroids[i]
+            if curr > 0:
+                stack.append(curr)
+            else:
+                while stack and stack[-1] > 0 and abs(curr) > stack[-1]:
+                    stack.pop()
+
+                if stack and abs(curr) == stack[-1]:
+                    stack.pop()
+
+                elif not stack or stack[-1] < 0:
+                    stack.append(curr)   
+
+        return stack
+                 
