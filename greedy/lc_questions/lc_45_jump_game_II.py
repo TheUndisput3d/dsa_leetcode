8ca49c329e76986d9148020c216909a785b2eb5a
Aayush <theundisputed.ze@gmail.com>
2025-07-16 11:00:07 +0545

    add greedy approach to lc-45 jump-game-II

--- greedy/lc_questions/lc_45_jump_game_II.py
+++ greedy/lc_questions/lc_45_jump_game_II.py
@@ -1,3 +1,27 @@
+## Brute Force Approach: Recursion
+# T.C: O(n!)
+# S.C: O(n)
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        def solve(index, jump):
+
+            if index >= len(nums)-1:
+                return jump
+            
+            min_jump = float('inf')
+
+            for i in range(1, nums[index]+1):
+                min_jump = min(min_jump, solve(index+i, jump+1))
+
+            return min_jump
+
+        solve(0, 0)
+        return solve(0, 0)
+
+
+## Recursion (DP - Memoization)
+# T.C: O(n^2)
+# S.C: O(n)
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
@@ -19,3 +43,26 @@ class Solution:
             return dp[index]
 
         return solve(0)
+
+
+## Optimal Approach
+# T.C: O(n)
+# S.C: O(1)
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        i = j = 0
+        farthest = 0
+        jump = 0
+
+        while j < n - 1:
+            for k in range(i, j+1):
+                farthest = max(farthest, k+nums[k])
+            i = j + 1
+            j = farthest
+            jump += 1
+            
+        return jump
+
+
