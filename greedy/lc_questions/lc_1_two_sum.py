33c6db9b8e8172df9f9248e9b42c9a99251de545
Aayush <theundisputed.ze@gmail.com>
2025-07-18 20:51:32 +0545

    add lc-45 two-sum

---
+++ greedy/lc_questions/lc_1_two_sum.py
@@ -0,0 +1,29 @@
+from typing import List
+
+## Brute Force
+# T.C: O(n^2)
+# S.C: O(1)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+        # Return an empty list if no solution is found
+        return []
+    
+
+## Optimal Solution
+# T.C: O(n)
+# S.C: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+        # If no valid pair is found, return an empty list
+        return []
