a5bcc36b5d87531977c6124198298b086ee610a3
Aayush <theundisputed.ze@gmail.com>
2025-07-19 20:28:01 +0545

    add min implementation using recursion

--- basic_recursion/min_max_in_array.py
+++ basic_recursion/min_max_in_array.py
@@ -1,11 +1,18 @@
 ## calculating max number using recursion
-
 def calcMax(arr, max_, i):
   if i == -1:
     return max_
   
   return calcMax(arr, max(max_, arr[i]), i-1)
 
+
+def calcMin(arr, min_, i):
+  if i == -1:
+    return min_
+
+  return calcMin(arr, min(min_, arr[i]), i-1)
+
 if __name__ == "__main__":
   calcMax([5, 4, 2, 1], -92838, 4-1)
+  calcMin([234, -345321, -1, -445], 9384523, 3)
 
